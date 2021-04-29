a6a47a4c1ec377a8ad92630518f4caa2abdae3d1
Andrew <androidengel@gmail.com>
2021-04-29 11:25:57 -0500

    Creating and configuring Link model

--- src/schema.js
+++ src/schema.js
@@ -1,9 +1,20 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-  type Query {}
+  type Link {
+    id: Int!
+    url: String!
+    slug: String!
+  }
 
-  type Mutation {}
+  type Query {
+    link(id: Int!): Link
+    allLinks: [Link!]!
+  }
+
+  type Mutation {
+    createLink(url: String!, slug: String!): Link!
+  }
 `;
 
 module.exports = typeDefs;
